303ea9aa9ca664274b694923c5e35878a2a36bea
ls123456789 <2953757467@qq.com>
2015-11-18 21:04:34 +0800

    Initial Commit

---
+++ student/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  student
+//
+//  Created by admin on 15/11/18.
+//  Copyright © 2015年 内师大. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
