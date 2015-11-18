8f697f968d2dfb4318728454f50901af67289d00
ls123456789 <2953757467@qq.com>
2015-11-18 21:22:18 +0800

    一个关于学生管理的程序
    定义一个学生类，类中包括学生的姓名学号性别电话号码课程等信息，再加入两个函数，分别用来输入和输出信息。

--- student/main.cpp
+++ student/main.cpp
@@ -7,6 +7,39 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+protected:
+    string name;
+    int num;
+    char sex;
+    string phnum;
+    string course[5];
+public:
+    Student(string n,int nu,char s,string pn,string cou[5])
+    {
+        name = n;
+        num = nu;
+        sex = s;
+        phnum = pn;
+        course[0] = cou[0];
+        course[1] = cou[1];
+        course[2] = cou[2];
+        course[3] = cou[3];
+        course[4] = cou[4];
+    }
+    void set_put()
+    {
+        cin>>name>>num>>sex>>phnum>>course[0]>>course[1]>>course[2]>>course[3]>>course[4]>>endl;
+    }
+    void show()
+    {
+        
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
